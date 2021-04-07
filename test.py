472f5cf82b4afe9a2289848588d34704dc86daaa
alokmatta <alokmatta@gmail.com>
2021-04-07 12:59:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import sys
 import shutil
+import os
+
 
 total, used, free = shutil.disk_usage("/")
 
 print("Total: %d GiB" % (total // (2**30)))
 print("Used: %d GiB" % (used // (2**30)))
 print("Free: %d GiB" % (free // (2**30)))
+
+print(sys.executable)
+print(os.environ)

613cf6b5adc86fb92085127a1575a9014edff057
alokmatta <alokmatta@gmail.com>
2021-04-07 12:51:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import sys
+import shutil
 
-import psutil
+total, used, free = shutil.disk_usage("/")
 
-hdd = psutil.disk_usage('/')
-
-print ("Total: %d GiB" % hdd.total / (2**30))
-print ("Used: %d GiB" % hdd.used / (2**30))
-print ("Free: %d GiB" % hdd.free / (2**30))
+print("Total: %d GiB" % (total // (2**30)))
+print("Used: %d GiB" % (used // (2**30)))
+print("Free: %d GiB" % (free // (2**30)))

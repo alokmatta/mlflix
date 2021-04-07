c70c3ae456cb2127e6e70db9f9b1c9eeaf077476
alokmatta <alokmatta@gmail.com>
2021-04-07 12:50:24 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+import psutil
+
+hdd = psutil.disk_usage('/')
+
+print ("Total: %d GiB" % hdd.total / (2**30))
+print ("Used: %d GiB" % hdd.used / (2**30))
+print ("Free: %d GiB" % hdd.free / (2**30))

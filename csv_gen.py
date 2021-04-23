291f0a25021fe4d825e84ef7567b30a64309f914
alokmatta <alokmatta@gmail.com>
2021-04-23 10:03:19 +0100

    Update csv_gen.py

--- csv_gen.py
+++ csv_gen.py
@@ -11,18 +11,22 @@ df_channels = pd.read_csv("channels.csv")
 
 df_list = []
 
-for i in range(0,len(df_channels)):
+#for i in range(0,len(df_channels)):
+for i in range(0,10)):
 
     if i%2:
         yt_key = yt_key2
+        key_str = "Key2"
+    else:
+        key_str = "Key1"
 
     url = "https://www.googleapis.com/youtube/v3/search?key="+yt_key + \
         "&channelId="+df_channels.channel_id[i]+"&part=snippet,id&order=date&maxResults=5&type=video"
     # print(url)
-    print("fetching channel", df_channels.name[i])
+    print("fetching channel", df_channels.name[i], "using", key_str)
     from urllib.request import urlopen
     json_str = urlopen(url).read()
     df_list.append(pd.json_normalize(json.loads(json_str)["items"]))
 
 df = pd.concat(df_list)
-df.to_csv("videos.csv")
+#df.to_csv("videos.csv")
